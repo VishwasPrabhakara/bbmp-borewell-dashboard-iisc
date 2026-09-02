aaba4c2f009190322a808372247e333a8c6be37e
VishwasPrabhakara <vp14032001@gmail.com>
2026-09-02 19:32:20 +0530

    Switch basemap from CARTO (now requires key) to OpenStreetMap

--- js/app.js
+++ js/app.js
@@ -95,9 +95,9 @@ function initMap() {
   map = L.map("map", { zoomControl: false, minZoom: 10, maxZoom: 18 }).setView([12.972, 77.594], 11);
   L.control.zoom({ position: "bottomleft" }).addTo(map);
   // Carto Positron - clean, minimal international-style basemap
-  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: "abcd", maxZoom: 20,
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19,
   }).addTo(map);
 }
 
